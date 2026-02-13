5e77d69bc01c96945558b1c855daf779c4316849
Nayan Islam <iamnayanislam@gmail.com>
2026-02-13 17:33:49 +0600

    explain how the code will be executed

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ console.log("than this");
 //Synchronous/Asynchronous: JavaScript reads your code line by line (synchronously) but it has the ability to handle asynchronous task. A synchronous language would be blocking. (it would wait for the time/setTimeout/data fetch to finish). But JavaScript uses eventloop to handle tasks asynchronously, so it's asynchronous language.
 
 //Finaly: JavaScript is a single threaded language that achieves non blocking, asynchronous behaviour using an event loop.
+
+setTimeout(() => {
+  console.log("This line will run after 0 seconds");
+}, 0); //This task will be sent to the task queue
+
+console.log("This line will run instantly!!!"); //this task will be sent to the call stack
+console.log("than this"); // this task also will be sent to the call stack
+//this two task will create a queue in the call stack the first task on line 26 will be executed first then the task on line 27 after that the call stack will be empty
+
+//after that the event loop will be notified that the call stack is empty then it will send the task from task queue to the call stack and the task inside setTimeout will be executed
